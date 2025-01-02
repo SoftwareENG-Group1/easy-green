15547c8e6afd2be8e5428c267f155fce03e9a38a
ise2005best <93008391+ise2005best@users.noreply.github.com>
2025-01-02 20:55:38 +0100

    fixed bugs

--- easy-green-backend/src/transactions/transactions.module.ts
+++ easy-green-backend/src/transactions/transactions.module.ts
@@ -3,9 +3,12 @@ import { TransactionsService } from './transactions.service';
 import { TransactionsController } from './transactions.controller';
 import { Transactions } from './entity/transactions.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { Borrower } from 'src/borrower/entity/borrower.entity';
+import { ContributionsModule } from 'src/contributions/contributions.module';
+import { MonthlyContributionsModule } from 'src/monthly-contributions/monthly-contributions.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Transactions])],
+  imports: [TypeOrmModule.forFeature([Transactions, Borrower]), ContributionsModule, MonthlyContributionsModule],
   providers: [TransactionsService],
   controllers: [TransactionsController]
 })
